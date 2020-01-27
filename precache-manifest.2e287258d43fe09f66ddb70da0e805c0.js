503627904154bad60352a60ca6d51a037be02f19
Tatu Wikman <tatu.wikman@gmail.com>
2020-01-27 14:59:31 +0200

    deploy

---
+++ precache-manifest.2e287258d43fe09f66ddb70da0e805c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60fbd4e5bfa917d61b0",
+    "url": "/matrix-viewer/css/chunk-vendors.9eda7404.css"
+  },
+  {
+    "revision": "e9c38366cc1520ef9b11",
+    "url": "/matrix-viewer/css/config.49c2f09e.css"
+  },
+  {
+    "revision": "d5f904a893d088bfa9f0d12c03f9df9f",
+    "url": "/matrix-viewer/index.html"
+  },
+  {
+    "revision": "adad1ca6e6e08567a0ac",
+    "url": "/matrix-viewer/js/about.6adb6280.js"
+  },
+  {
+    "revision": "a6705808dd3452f82fcc",
+    "url": "/matrix-viewer/js/app.f483faf3.js"
+  },
+  {
+    "revision": "a60fbd4e5bfa917d61b0",
+    "url": "/matrix-viewer/js/chunk-vendors.5ab6f120.js"
+  },
+  {
+    "revision": "e9c38366cc1520ef9b11",
+    "url": "/matrix-viewer/js/config.51cffa34.js"
+  },
+  {
+    "revision": "24da70713f35096a2d5b6b23ecbda256",
+    "url": "/matrix-viewer/lib/matrix.js"
+  },
+  {
+    "revision": "cc8ddba85f5fce5e496c375a74e0132b",
+    "url": "/matrix-viewer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/matrix-viewer/robots.txt"
+  }
+]);
